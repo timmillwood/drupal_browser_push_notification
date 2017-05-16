ca6533573af637911b5da5bd8cd735c420480b8f
shylaja <shylaja.r@iksula.com>
2017-05-16 12:43:06 +0530

    serviceworker

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -4,35 +4,35 @@
  */
 'use strict';
 self.addEventListener('push', function (event) {
- 'use strict';   
+  'use strict';  
   if (!(self.Notification && self.Notification.permission === 'granted')) {
-      return;
-    }
+    return;
+  }
 // console.log('push received');
   const sendNotification = body => {
-  var str = body;
-  var message_array = str.split('<br>');
-  var notificationOptions = {
-    body: message_array[1],
-    icon: message_array[2],
-    badge: message_array[2],
-    tag: message_array[2],
-    data: {
-      url: message_array[3]
-    }
-  };
-  return self.registration.showNotification(message_array[0],
-            notificationOptions);
+    var str = body;
+    var message_array = str.split('<br>');
+    var notificationOptions = {
+      body: message_array[1],
+      icon: message_array[2],
+      badge: message_array[2],
+      tag: message_array[2],
+      data: {
+        url: message_array[3]
+      }
     };
+    return self.registration.showNotification(message_array[0],
+            notificationOptions);
+  };
 
-    if (event.data) {
-        const message = event.data.text();
-        event.waitUntil(sendNotification(message));
-    }
+  if (event.data) {
+    const message = event.data.text();
+    event.waitUntil(sendNotification(message));
+  }
 });
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
- console.log('notification click');
+  // console.log('notification click');
   let clickResponsePromise = Promise.resolve();
   if (event.notification.data && event.notification.data.url) {
     clickResponsePromise = clients.openWindow(event.notification.data.url);
